1fb5bca91f338fcd75d814bdf97b33f79a03ced3
shaantanu314 <shaantanu2018@gmail.com>
2023-01-18 17:57:23 +0530

    add: local frame top down

--- pcdGenerate.py
+++ pcdGenerate.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 import os
 
-def registration_result(source, target, transformation):
-    target.transform(transformation)
+def registration_result(source, target):
+    # target.transform(transformation)
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(np.vstack((np.asarray(source.points), np.asarray(target.points))))
@@ -12,7 +12,7 @@ def registration_result(source, target, transformation):
 
     return pcd
 
-
+# def mergePCD(source,target)
 
 def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
     '''
@@ -24,6 +24,7 @@ def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
     scans_list = os.listdir(posepath)
     scans_list = list(map(lambda x: x.split('.')[0],scans_list))
 
+
     pcd_list = np.empty((0,3))
     color_list = np.empty((0,3))
 
@@ -31,10 +32,12 @@ def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
 
     reg_p2p = None
 
+    latest_registrations = []
+
     for i,scan in enumerate(scans_list):
-        
+        print(i)
         depth_file = depthpath+"/"+scan+'.npy'
-        image_file = imgpath+'/'+scan[:-4]+'.png'
+        image_file = imgpath+'/'+scan+'.png'
 
         if(not os.path.exists(depth_file)):
             continue
@@ -42,7 +45,6 @@ def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
         if(not os.path.exists(image_file)):
             continue
         
-        # print(i)
 
         try:
             depth_map = o3d.geometry.Image(np.ascontiguousarray(np.load(depth_file)).astype(np.float32))
@@ -60,7 +62,7 @@ def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
         pcd.transform(pose)
 
         # Take the each 25ht point of the pointcloud
-        pcd = pcd.uniform_down_sample(25)
+        pcd = pcd.uniform_down_sample(100)
 
         # Set initial transformation and threshold. May depend on previous params
         threshold = 0.005
@@ -70,13 +72,24 @@ def generatePointCloud(imgpath,depthpath,posepath, cameraIntrinsicMatrix):
                                  [0.0, 0.0, 0.0, 1.0]])
 
         if reg_p2p is not None and np.asarray(reg_p2p.points).shape != 0:
+
+            latest_pcd = o3d.geometry.PointCloud()
+            for pcd_curr in latest_registrations:
+                latest_pcd = registration_result(latest_pcd, pcd_curr)
+
             reg_res = o3d.pipelines.registration.registration_icp(
-                    reg_p2p, pcd, threshold, trans_init,
+                    latest_pcd, pcd, threshold, trans_init,
                     o3d.pipelines.registration.TransformationEstimationPointToPoint())
             transforms.append(reg_res.transformation)
-            reg_p2p = registration_result(reg_p2p, pcd, reg_res.transformation)
+
+            transformed_pcd = pcd.transform(reg_res.transformation)
+            latest_registrations.append(transformed_pcd)
+            reg_p2p = registration_result(reg_p2p, transformed_pcd)
         else:
             reg_p2p = pcd
+
+        if(len(latest_registrations)>50):
+            latest_registrations = latest_registrations[1:]
 
         color_list = np.vstack((color_list,np.asarray(pcd.colors)))
         pcd_list = np.vstack((pcd_list,np.asarray(pcd.points)))
